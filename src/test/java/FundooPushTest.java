a7c2fdc5005733f0a1a246473de778b8539fba83
luckyNB <20150773@dbatu.ac.in>
2019-12-30 19:45:58 +0530

    Added test case for adding redirect

--- src/test/java/FundooPushTest.java
+++ src/test/java/FundooPushTest.java
@@ -9,6 +9,8 @@ import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 public class FundooPushTest {
 
     @Test
@@ -16,7 +18,6 @@ public class FundooPushTest {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("email", "laxmanbhosale7374@gmail.com");
         jsonObject.put("password", "123456");
-
         Response response = RestAssured.given()
                 .body(jsonObject.toJSONString())
                 .when()
@@ -27,9 +28,6 @@ public class FundooPushTest {
         JSONObject object = (JSONObject) new JSONParser().parse(body.prettyPrint());
         boolean status = (boolean) object.get("status");
         String message = (String) object.get("message");
-        System.out.println("Status:" + status);
-        System.out.println("status code:" + code);
-        System.out.println();
         Assert.assertEquals(200, code);
         Assert.assertTrue(status);
         Assert.assertEquals("Logged in Successfully", message);
@@ -69,9 +67,6 @@ public class FundooPushTest {
         JSONObject object = (JSONObject) new JSONParser().parse(body.prettyPrint());
         boolean status = (boolean) object.get("status");
         String message = (String) object.get("message");
-        System.out.println("Status:" + status);
-        System.out.println("status code:" + code);
-        System.out.println();
         Assert.assertEquals(401, code);
         Assert.assertFalse(status);
         Assert.assertEquals("Wrong password", message);
@@ -92,9 +87,6 @@ public class FundooPushTest {
         JSONObject object = (JSONObject) new JSONParser().parse(body.prettyPrint());
         boolean status = (boolean) object.get("status");
         String message = (String) object.get("message");
-        System.out.println("Status:" + status);
-        System.out.println("status code:" + code);
-        System.out.println();
         Assert.assertEquals(401, code);
         Assert.assertFalse(status);
         Assert.assertEquals("Wrong password", message);
@@ -115,7 +107,53 @@ public class FundooPushTest {
         Assert.assertTrue(status);
         Assert.assertEquals(200, code);
         Assert.assertEquals("Logged out successfully from the system", message);
+    }
+
+    @Test
+    public void givenTokenAndUserDetails_WhenCorrect_ShouldChangePassword() throws ParseException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("user_id", "5e0985c14d22670032530f13");
+        jsonObject.put("email", "laxmanbhosale7374@gmail.com");
+        jsonObject.put("password", "123456");
+        jsonObject.put("new_password", "abcd1234");
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .header(new Header("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjVlMDk4NWMxNGQyMjY3MDAzMjUzMGYxMyJ9LCJpYXQiOjE1Nzc2ODI0OTMsImV4cCI6MTU3Nzc2ODg5M30.jh2CJZdfLQ47GLDzJ5HOCplljRcb9hmJ3V6-51US8Kk"))
+                .body(jsonObject.toJSONString())
+                .post("https://fundoopush-backend-dev.bridgelabz.com/account/change-password");
+        ResponseBody body = response.getBody();
+        JSONObject object = (JSONObject) new JSONParser().parse(body.prettyPrint());
+        boolean status = (boolean) object.get("status");
+        String message = (String) object.get("message");
+        System.out.println(status + "    " + message);
+        int code = response.getStatusCode();
+        Assert.assertEquals(200, code);
+    }
 
 
+    @Test
+    public void givenToken_Image_TitleAndDescriptionForPost_RedirectionLinks_isPublished_isArchieved_youTubeFlag_youtubelink_WhenCorrect_ShouldReturnTrueAndSuccessMessage() throws ParseException {
+
+        File testUploadFile = new File("/home/user/workspace/FundooPushTest/src/test/resources/demo.jpg");
+        Response response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjVlMDk4NWMxNGQyMjY3MDAzMjUzMGYxMyJ9LCJpYXQiOjE1Nzc3MTE1NzAsImV4cCI6MTU3Nzc5Nzk3MH0.yocUdD36Uenhb3amGDoXP_fMcPEAbBrTIIhU-iJZwYg")
+                .contentType("multipart/form-data")
+                .multiPart("multipart/", new File("/home/user/workspace/FundooPushTest/src/test/resources/demo.jpg"))
+                .formParam("title", "ganeshji")
+                .formParam("description", "goddemo ganesh")
+                .formParam("redirect_link", "www.google.com")
+                .formParam("is_published", false)
+                .formParam("archive", false)
+                .formParam("youtube_flag", false)
+                .formParam("youtube_url", false)
+                .formParam("video_link", false)
+                .when()
+                .post("https://fundoopush-backend-dev.bridgelabz.com/redirects");
+        ResponseBody body = response.getBody();
+        JSONObject object = (JSONObject) new JSONParser().parse(body.prettyPrint());
+        System.out.println(body.prettyPrint());
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(200, statusCode); // response.then().
     }
 }
